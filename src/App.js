b0842128232dc9ed13665d9b54abfcf7b53c6b3a
Tshembani04 <aurthurmhlongo.ta@gmail.com>
2021-08-01 22:15:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,47 @@
-import React from 'react';
-import { Cards, Chart, CountryPicker } from './components';
-import styles from './App.module.css';
-import { fetchData } from './api'
-
- 
+import React from "react";
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
+import image from './assets/image.png'
 
 class App extends React.Component {
-    state={
-        //data is just an object until is populated
-        data: {},
-        country: '',
-    }
+  state = {
+    //data is just an object until is populated
+    data: {},
+    country: "",
+  };
 
-    async componentDidMount (){
-        const fetchedData = await fetchData();
+  async componentDidMount() {
+    const fetchedData = await fetchData();
 
-        //Populate data here, we set data to populate data ie data = fetchedData .
-        //We can now pass data as props into our Cards component
-            this.setState({ data: fetchedData });
-    }
+    //Populate data here, we set data to populate data ie data = fetchedData .
+    //We can now pass data as props into our Cards component
+    this.setState({ data: fetchedData });
+  }
 
-    handleCountryChange = async(country) =>{
-        const fetchedData = await fetchData(country);
-        console.log(fetchedData)
-        // console.log(country)
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    console.log(fetchedData);
+    // console.log(country)
 
-        //Fetch Data
-        //Set the state
-        this.setState({ data: fetchedData, country: country });
+    //Fetch Data
+    //Set the state
+    this.setState({ data: fetchedData, country: country });
+  };
 
-
-    }
-
-    render(){
-
-     //destructure data in fact take it out of this.state 
-        const { data, country } = this.state;
+  render() {
+    //destructure data in fact take it out of this.state
+    const { data, country } = this.state;
 
     return (
-        <div className={styles.container} >
-            <Cards data= { data } /> 
-            <CountryPicker handleCountryChange={this.handleCountryChange} />
-            <Chart data ={data} country={country}/>
-        </div>
-    )
-}}
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
+}
 
 export default App;
