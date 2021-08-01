a5e098d494ec3f8bbfcc4f7c7a7c0dc324a5ede9
Tshembani04 <aurthurmhlongo.ta@gmail.com>
2021-08-01 22:24:17 +0200

    first commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export { default as Cards } from './Cards/Cards.jsx';
-export { default as Chart } from './Chart/Chart.jsx';
-export { default as CountryPicker } from './CountryPicker/CountryPicker.jsx'
+export { default as Cards } from './Cards/Cards';
+export { default as Chart } from './Chart/Chart';
+export { default as CountryPicker } from './CountryPicker/CountryPicker'
